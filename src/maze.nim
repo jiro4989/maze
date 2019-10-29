fbc893964b5ed30759154329e77a6bb8ba49623e
jiro4989 <jiroron666@gmail.com>
2019-10-30 00:01:20 +0900

    バグ発見

--- src/maze.nim
+++ src/maze.nim
@@ -1,9 +1,10 @@
-import mazepkg/pulling_pole_down
+#import mazepkg/pulling_pole_down
+import mazepkg/dig
 
 when isMainModule:
   let
     width = 21
     height = 21
-  var maze = newMazeByPollingPoleDown(width, height)
-  echo maze.format(" ", "#")
+  var maze = newMazeByDigging(width, height)
+  #echo maze.format(" ", "#")
 
--- src/mazepkg/dig.nim
+++ src/mazepkg/dig.nim
@@ -8,6 +8,7 @@
 ## * `迷路自動生成アルゴリズム <http://www5d.biglobe.ne.jp/stssk/maze/make.html>`_
 
 import sequtils, strutils, random
+import os
 
 const
   road = 0'u8
@@ -19,6 +20,12 @@ type
     width*, height*: int
   Pos = object
     x, y: int
+
+proc `$`*(self: Maze): string =
+  var rows: seq[string]
+  for row in self.stage:
+    rows.add(row.mapIt($it).join())
+  result = rows.join("\n")
 
 proc newMazeWithFilledWall(width, height: int): Maze =
   result.width = width
@@ -34,18 +41,20 @@ proc setRoadFrame(maze: var Maze) =
   # top
   let stage = maze.stage
   for x, col in stage[0]:
-    stage[0][x] = road
+    maze.stage[0][x] = road
   # left
   for y, row in stage:
-    stage[y][0] = road
+    maze.stage[y][0] = road
   # right
   for y, row in stage:
-    stage[y][^1] = road
+    maze.stage[y][^1] = road
   # bottom
   for x, col in stage[^1]:
-    stage[^1][x] = road
+    maze.stage[^1][x] = road
 
-proc randDig(maze: var Maze, x, y: int) =
+proc randDig(maze: var Maze, pos: Pos): Pos =
+  let x = pos.x
+  let y = pos.y
   let r = rand(4)
   var x2, y2: int
   var x3, y3: int
@@ -55,17 +64,17 @@ proc randDig(maze: var Maze, x, y: int) =
     x2 = x
     x3 = x
     y2 = y - 1
-    y3 = y - 1
+    y3 = y - 2
   of 1:
     # left
     x2 = x - 1
-    x3 = x - 1
+    x3 = x - 2
     y2 = y
     y3 = y
   of 2:
     # right
     x2 = x + 1
-    x3 = x + 1
+    x3 = x + 2
     y2 = y
     y3 = y
   else:
@@ -73,23 +82,27 @@ proc randDig(maze: var Maze, x, y: int) =
     x2 = x
     x3 = x
     y2 = y + 1
-    y3 = y + 1
+    y3 = y + 2
   let stage = maze.stage
   let cell = stage[y2][x2]
   let cell2 = stage[y3][x3]
   if cell == wall and cell2 == wall:
     maze.stage[y2][x2] = road
+    result.x = x2
+    result.y = y2
 
-proc isDiggable(maze: Maze, x, y: int): bool =
+proc isDiggable(maze: Maze, pos: Pos): bool =
+  let x = pos.x
+  let y = pos.y
   let stage = maze.stage
   # top
   if stage[y-1][x] == wall and stage[y-2][x] == wall:
     return true
   # left
-  if stage[y][x-1] == wall and stage[2][x-2] == wall:
+  if stage[y][x-1] == wall and stage[y][x-2] == wall:
     return true
   # right
-  if stage[y][x+1] == wall and stage[2][x+2] == wall:
+  if stage[y][x+1] == wall and stage[y][x+2] == wall:
     return true
   # buttom
   if stage[y+1][x] == wall and stage[y+2][x] == wall:
@@ -106,8 +119,20 @@ proc isContinuableToDig(maze: Maze): bool =
   ## 配置可能な全て載せるのdiggableをチェック
   for y in 2..<maze.height-2:
     for x in 2..<maze.width-2:
-      if maze.isDiggable(x, y):
+      if maze.isDiggable(Pos(x: x, y: y)):
         return true
+
+proc digging(maze: var Maze, pos: Pos) =
+  var p = pos
+  while maze.isDiggable(p):
+    # FIXME: ここ実装勘違いしてた
+    p = maze.randDig(p)
+    var zero: Pos
+    if p == zero:
+      return
+    echo maze
+    echo $p.x & "-" & $p.y
+    sleep 300
 
 proc newMazeByDigging*(width, height: int): Maze =
   ## 穴掘り法で迷路を生成する。
@@ -118,4 +143,7 @@ proc newMazeByDigging*(width, height: int): Maze =
   randomize()
   while result.isContinuableToDig():
     var pos = result.newStartPos()
-    break
+    if pos.x mod 2 == 1 or pos.y mod 2 == 1:
+      continue
+    result.digging(pos)
+
